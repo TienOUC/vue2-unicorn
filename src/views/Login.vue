022e18fbc094104e7d61a4d05e9f4ab3192a2e6d
tienouc <willtien@outlook.com>
2021-09-24 12:32:49 +0800

    init

---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="login">This is Login page!</div>
+</template>
+
+<script>
+	export default {
+		name: 'Login',
+		components: {},
+		props: {},
+		data() {
+			return {};
+		},
+		watch: {},
+		computed: {},
+		methods: {},
+		created() {},
+		mounted() {},
+	};
+</script>
+
+<style lang="scss" scoped></style>
